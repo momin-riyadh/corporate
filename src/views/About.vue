203fdd1c6572737de5de0c21174fe2d0c119155c
momin-ctg <you@example.com>
2019-10-22 11:33:55 +0600

    fix and deploy

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,38 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="sq-about-area">
+    <Navigation/>
+    <CommonBanner v-bind:image-url="require('../assets/images/about.jpg')"
+                  banner-title="Who we are" banner-subtitle="SQUARE today symbolizes a name – a state of mind. But its journey to the
+growth and prosperity has been no bed of roses."/>
+
+    <div class="sq-about-main">
+      <div class="container-fluid px-5-percent">
+
+      </div>
+    </div>
+
+
+    <Footer/>
+    <Copyright/>
   </div>
 </template>
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+    import Navigation from "@/components/Navigation.vue";
+    import CommonBanner from "@/components/CommonBanner.vue";
+    import Footer from "@/components/Footer.vue";
+    import Copyright from "@/components/Copyright.vue";
+
+    @Component({
+        name: 'About',
+        components: {Copyright, Footer, CommonBanner, Navigation},
+    })
+
+    export default class About extends Vue {
+
+    }
+
+</script>
+<style scoped lang="scss">
+
+</style>
