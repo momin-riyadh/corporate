a27569cfa382cf994ef8488f95a1c857aecc2f0a
momin-ctg <you@example.com>
2019-10-23 14:47:22 +0600

    favicon added

--- src/router.ts
+++ src/router.ts
@@ -69,7 +69,7 @@ export default new Router({
             // route level code-splitting
             // this generates a separate chunk (about.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import(/* webpackChunkName: "about" */ './views/About.vue'),
+            component: () => import(/* webpackChunkName: "about" */ './views/about/About.vue'),
         },
     ],
 });
