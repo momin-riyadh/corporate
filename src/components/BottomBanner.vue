78d20fa0f0aa801c7d60dd8fbc28a0065120d624
momin-ctg <you@example.com>
2020-01-28 12:45:30 +0600

    nothing special

--- src/components/BottomBanner.vue
+++ src/components/BottomBanner.vue
@@ -3,7 +3,7 @@
     <div class="container-fluid px-5-percent">
       <div class="bottom-banner">
         <img class="img-fluid" :src="BottomImage" alt="">
-        <div class="col-md-6 mx-auto bottom-banner-text text-center">
+        <div class="col-6 mx-auto bottom-banner-text text-center">
           <h1>
             {{BottomTitle}}
           </h1>
@@ -11,7 +11,7 @@
 
 
         <div class="row justify-content-center">
-          <div class="col-xl-10">
+          <div class="col-xl-10 col-sm-10">
             <div class="bottom-paragraph-text text-center">
               <p>{{BottomText}}</p>
             </div>
@@ -26,7 +26,6 @@
 </template>
 
 <script lang="ts">
-
     import {Component, Vue, Prop} from 'vue-property-decorator';
 
     @Component({
@@ -43,47 +42,47 @@
 
 <style scoped lang="scss">
   .bottom-banner-area {
-
+    overflow: hidden;
   }
 
   .bottom-banner {
-    position: relative;
-    height: 50vh;
     display: block;
-    margin-bottom:250px;
-    &::before{
-      width: 100%;
-      height: 100%;
-      position: absolute;
-      content: "";
-      top: 0;
-      left: 0;
-      right: 0;
-      bottom: 0;
-      background: -webkit-gradient(linear, left top, left bottom, color-stop(100%, rgba(0, 0, 0, 0.3)), color-stop(0%, rgba(0, 0, 0, 0.3)));
-      background: linear-gradient(to bottom, rgba(0, 0, 0, 0.3) 100%, rgba(0, 0, 0, 0.3) 0%);
-    }
-
+    position: relative;
+    height: 100%;
     img {
-      height: 100%;
+      position: relative;
+      height: 400px;
       width: 100%;
       object-fit: cover;
+
+      &::before {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        content: "";
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        background: -webkit-gradient(linear, left top, left bottom, color-stop(100%, rgba(0, 0, 0, 0.3)), color-stop(0%, rgba(0, 0, 0, 0.3)));
+        background: linear-gradient(to bottom, rgba(0, 0, 0, 0.3) 100%, rgba(0, 0, 0, 0.3) 0%);
+      }
     }
   }
 
   .bottom-banner-text {
     position: absolute;
     left: 50%;
-    top: 40%;
-    -webkit-transform: translate(-50%, -60%);
-    -moz-transform: translate(-50%, -60%);
-    -ms-transform: translate(-50%, -60%);
-    -o-transform: translate(-50%, -60%);
-    transform: translate(-50%, -60%);
+    top: 20%;
+    -webkit-transform: translateX(-50%);
+    -moz-transform: translateX(-50%);
+    -ms-transform: translateX(-50%);
+    -o-transform: translateX(-50%);
+    transform: translateX(-50%);
 
     h1 {
       color: $white-color;
-      font-size: calc(16px + (50 - 16) * ((100vw - 300px) / (1600 - 300)));;
+      font-size: calc(16px + (42 - 16) * ((100vw - 300px) / (1600 - 300)));;
     }
 
   }
@@ -91,12 +90,20 @@
   .bottom-paragraph-text {
     margin-top: -100px;
     background: #F4F4F4;
-    padding: 4rem;
+    padding: 1rem;
     color: $paragraph-color;
     font-size: $paragraph-size;
-    p{
+    @media(min-width: 601px) {
+      padding: 4rem;
+    }
+
+    p {
       font-family: $font-roboto;
       line-height: 2em;
+      font-size: .9rem;
+      @media(min-width: 601px) {
+        font-size: 1.1rem;
+      }
     }
   }
 </style>
