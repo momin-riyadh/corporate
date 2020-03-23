18e742fcb9029fa16af42ffc7637aa8a766eb008
mominriyadh <riyadh@codesign.com.bd>
2020-03-23 12:02:15 +0600

    home page component has been seperate

---
+++ src/components/ComplianceCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container-fluid px-5-percent overflow-hidden">
+    <div class="sq-home-compliance-area">
+      <img src="../assets/images/compliance.jpg" alt="" class="img-fluid">
+      <div class="compliance-text">
+        <h1 class="mb-2 mb-md-3">Compliance</h1>
+        <h4 class="mb-3 mb-md-4">It's a team work.</h4>
+        <p>
+          Gaining an understanding of local markets is as important as monitoring the greater economy. We retain our
+          core belief that residential property is more than just aestment, so we learn about the impact on local
+          areas before investing.
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+    export default {
+        name: "ComplianceCard"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,22 +3,7 @@
     <Navigation type="customstyle"/>
     <VideoCard/>
     <OppertunityCard/>
-
-    <div class="container-fluid px-5-percent overflow-hidden">
-      <div class="sq-home-compliance-area">
-        <img src="../assets/images/compliance.jpg" alt="" class="img-fluid">
-        <div class="compliance-text">
-          <h1 class="mb-2 mb-md-3">Compliance</h1>
-          <h4 class="mb-3 mb-md-4">It's a team work.</h4>
-          <p>
-            Gaining an understanding of local markets is as important as monitoring the greater economy. We retain our
-            core belief that residential property is more than just aestment, so we learn about the impact on local
-            areas before investing.
-          </p>
-        </div>
-      </div>
-    </div>
-
+    <MissionCard/>
 
     <!--Square home page bottom slider component for latest news views-->
     <div class="sq-h-news-area">
@@ -179,10 +164,12 @@
     import {swiper, swiperSlide} from 'vue-awesome-swiper';
     import VideoCard from '@/components/VideoCard.vue';
     import OppertunityCard from "@/components/OppertunityCard.vue";
+    import MissionCard from "@/components/MissionCard.vue";
 
     @Component({
         name: 'Home',
         components: {
+            MissionCard,
             OppertunityCard,
             VideoCard,
             CommonBanner,
