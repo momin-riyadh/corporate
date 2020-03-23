7e0ef59ea632894caccd17248f17ccd7078c6194
mominriyadh <riyadh@codesign.com.bd>
2020-03-23 11:57:07 +0600

    home page component has been seperate

---
+++ src/components/OppertunityCard.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="container-fluid px-5-percent">
+    <div class="sq-h-content-area">
+      <div class="row align-items-center">
+        <div class="col-lg-6">
+          <div class="sq-h-content">
+            <h1 class="sq-h-content-title mb-4"> <span>Identifying Opportunities <br/>
+              Creating Values</span></h1>
+            <p>Gaining an understanding of local markets is as important as monitoring the greater economy. We retain
+              our core belief that residential property is more than just an investment, so we learn about the impact
+              on
+              local areas before investing.</p>
+          </div>
+          <div class="sq-logo-bg">
+            <img src="../assets/images/sq-logobg.svg" alt="square">
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <img src="../assets/images/h-top.jpg" alt="" class="img-fluid float-right">
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+
+    @Component({
+        name: 'OppertunityCard',
+        components: {}
+    })
+
+    export default class OppertunityCard extends Vue {
+
+    }
+</script>
+
+<style scoped lang="scss">
+  .sq-h-content-area {
+    position: relative;
+    overflow: hidden;
+    margin: calc(3% + 5vh) 0;
+  }
+
+  .sq-h-content {
+    p {
+      margin-left: 0;
+      color: #696969;
+      font-family: $font-roboto;
+      max-width: 600px;
+      line-height: 2em;
+      position: relative;
+      top: 0;
+      @media(min-width: 601px) {
+        margin-left: 150px;
+        color: #696969;
+        font-family: $font-roboto;
+        max-width: 600px;
+        line-height: 2em;
+        position: relative;
+        top: 80px;
+      }
+    }
+  }
+
+  .sq-h-content-title {
+    line-height: 1.2;
+    font-family: $font-helvetica-bold;
+    font-weight: bold;
+    font-size: calc(24px + (45 - 24) * ((100vw - 300px) / (1600 - 300)));
+    margin-left: 0;
+    position: relative;
+    @media(min-width: 601px) {
+      line-height: 1.2;
+      font-family: $font-helvetica-bold;
+      font-weight: bold;
+      font-size: calc(24px + (45 - 24) * ((100vw - 300px) / (1600 - 300)));
+      margin-left: 95px;
+      position: relative;
+    }
+
+    &::before {
+      content: "";
+      position: absolute;
+      height: 5px;
+      border-bottom: 4px solid $brand-color;
+      top: 30%;
+      width: 80px;
+      right: 100%;
+      margin-right: 15px;
+      display: none;
+      @media(min-width: 601px) {
+        content: "";
+        position: absolute;
+        height: 5px;
+        border-bottom: 4px solid $brand-color;
+        top: 30%;
+        width: 80px;
+        right: 100%;
+        margin-right: 15px;
+        display: block;
+      }
+    }
+
+    span {
+      display: inline-block;
+      position: relative;
+    }
+  }
+
+  .sq-logo-bg {
+    display: flex;
+    justify-content: flex-end;
+    flex-direction: column;
+    align-items: flex-start;
+    z-index: -1;
+    bottom: 120px;
+    position: relative;
+    @media (max-width: 600px) {
+      display: none;
+    }
+
+    img {
+      width: 400px;
+      height: 400px;
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,77 +2,7 @@
   <div class="sq-home-area">
     <Navigation type="customstyle"/>
     <VideoCard/>
-
-    <div class="container-fluid px-5-percent">
-      <div class="sq-h-content-area">
-        <div class="row align-items-center">
-          <div class="col-lg-6">
-            <div class="sq-h-content">
-              <h1 class="sq-h-content-title mb-4"> <span>Identifying Opportunities <br/>
-              Creating Values</span></h1>
-              <p>Gaining an understanding of local markets is as important as monitoring the greater economy. We retain
-                our core belief that residential property is more than just an investment, so we learn about the impact
-                on
-                local areas before investing.</p>
-            </div>
-            <div class="sq-logo-bg">
-              <img src="../assets/images/sq-logobg.svg" alt="square">
-            </div>
-          </div>
-          <div class="col-lg-6">
-            <img src="../assets/images/h-top.jpg" alt="" class="img-fluid float-right">
-          </div>
-        </div>
-      </div>
-    </div>
-
-
-    <div class="container-fluid px-5-percent my-5">
-      <div class="sq-mission-area">
-        <div class="sq-mission-title">
-          <h1>Started its journey</h1>
-          <p>in 1988 and in 1994</p>
-        </div>
-
-        <div class="row">
-          <div class="col text-center">
-            <div class="row text-left pt-4">
-              <div class="col-12 col-lg-4 mb-4 sq-mission-text pr-0 pr-md-5">
-                <h3>Our Mission</h3>
-                <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live
-                  the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large
-                  language ocean. A small river named Duden
-                  flows by their place far far away.
-                </p>
-              </div>
-              <div class="col-12 col-lg-4 sq-mission-text pl-3 pl-lg-5">
-                <h3>Our Vision</h3>
-                <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, far far away, behind the
-                  word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated
-                  they live in Bookmarksgrove right at
-                  the coast.
-                </p>
-              </div>
-
-              <div class="col-12 col-lg-4 sq-mission-text pl-3 pl-lg-5">
-                <h3>Our Values</h3>
-                <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, far far away, behind the
-                  word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated
-                  they live in Bookmarksgrove right at
-                  the coast.
-                </p>
-              </div>
-            </div>
-            <div class="sq-mission-img col-12 p-0">
-              <img src="../assets/images/mission-ref.jpg" alt="" class="img-fluid">
-            </div>
-          </div>
-        </div>
-
-
-      </div>
-    </div>
-
+    <OppertunityCard/>
 
     <div class="container-fluid px-5-percent overflow-hidden">
       <div class="sq-home-compliance-area">
@@ -248,10 +178,12 @@
     import CommonBanner from '@/components/CommonBanner.vue';
     import {swiper, swiperSlide} from 'vue-awesome-swiper';
     import VideoCard from '@/components/VideoCard.vue';
+    import OppertunityCard from "@/components/OppertunityCard.vue";
 
     @Component({
         name: 'Home',
         components: {
+            OppertunityCard,
             VideoCard,
             CommonBanner,
             Copyright,
@@ -303,166 +235,6 @@
 </script>
 
 <style lang="scss">
-  .sq-h-content-area {
-    position: relative;
-    min-height: 550px;
-    overflow: hidden;
-    margin: calc(3% + 5vh) 0;
-  }
-
-  .sq-h-content {
-    p {
-      margin-left: 0;
-      color: #696969;
-      font-family: $font-roboto;
-      max-width: 600px;
-      line-height: 2em;
-      position: relative;
-      top: 0;
-      @media(min-width: 601px) {
-        margin-left: 150px;
-        color: #696969;
-        font-family: $font-roboto;
-        max-width: 600px;
-        line-height: 2em;
-        position: relative;
-        top: 80px;
-      }
-    }
-  }
-
-  .sq-h-content-title {
-    line-height: 1.2;
-    font-family: $font-helvetica-bold;
-    font-weight: bold;
-    font-size: calc(24px + (45 - 24) * ((100vw - 300px) / (1600 - 300)));
-    margin-left: 0;
-    position: relative;
-    @media(min-width: 601px){
-      line-height: 1.2;
-      font-family: $font-helvetica-bold;
-      font-weight: bold;
-      font-size: calc(24px + (45 - 24) * ((100vw - 300px) / (1600 - 300)));
-      margin-left: 95px;
-      position: relative;
-    }
-
-    &::before {
-      content: "";
-      position: absolute;
-      height: 5px;
-      border-bottom: 4px solid $brand-color;
-      top: 30%;
-      width: 80px;
-      right: 100%;
-      margin-right: 15px;
-      display: none;
-      @media(min-width: 601px){
-        content: "";
-        position: absolute;
-        height: 5px;
-        border-bottom: 4px solid $brand-color;
-        top: 30%;
-        width: 80px;
-        right: 100%;
-        margin-right: 15px;
-        display: block;
-      }
-    }
-
-    span {
-      display: inline-block;
-      position: relative;
-    }
-  }
-
-  .sq-logo-bg {
-    display: flex;
-    justify-content: flex-end;
-    flex-direction: column;
-    align-items: flex-start;
-    z-index: -1;
-    bottom: 120px;
-    position: relative;
-    @media (max-width: 600px) {
-      display: none;
-    }
-
-    img {
-      width: 400px;
-      height: 400px;
-    }
-  }
-
-
-  .sq-mission-area {
-    background: #F4F4F4;
-    padding: 30px;
-    margin-bottom: calc(3% + 5vh);
-    @media(min-width: 600px) {
-      padding: 95px;
-    }
-  }
-
-  .sq-mission-title {
-    h1 {
-      line-height: 1;
-      font-family: $font-helvetica-bold;
-      font-weight: bold;
-      font-size: calc(24px + (45 - 24) * ((100vw - 300px) / (1600 - 300)));
-      display: inline-block;
-      position: relative;
-
-      &::before {
-        content: "";
-        position: absolute;
-        height: 0;
-        border-bottom: 4px solid $brand-color;
-        top: 50%;
-        transform: translateY(-50%);
-        width: 0;
-        right: 100%;
-        margin-right: 15px;
-        @media(min-width: 600px) {
-          width: 80px;
-          height: 5px;
-        }
-      }
-    }
-
-    p {
-      font-family: $font-helvetica-bold;
-      font-size: calc(18px + (22 - 18) * ((100vw - 300px) / (1600 - 300)));
-      color: #777777;
-    }
-  }
-
-  .sq-mission-text {
-    h3 {
-      font-family: $font-roboto;
-      font-weight: bold;
-      color: $header-color;
-    }
-
-    p {
-      font-family: $font-roboto;
-      color: $paragraph-color;
-      line-height: 1.8;
-
-    }
-  }
-
-  .sq-mission-img {
-    img {
-      height: 55vh;
-      width: 100%;
-      object-fit: cover;
-      -o-object-fit: cover;
-      object-position: center;
-      -o-object-position: center;
-    }
-  }
-
 
   .sq-home-compliance-area {
     height: 70vh;
