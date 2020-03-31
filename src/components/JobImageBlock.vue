8e4fc7fc738817caca8aee1ffb48afbfa3cec933
mominriyadh <riyadh@codesign.com.bd>
2020-03-31 16:25:52 +0600

    fix and deploy

---
+++ src/components/JobImageBlock.vue
@@ -0,0 +1,82 @@
+<template>
+  <!--    Bottom Image Grid-->
+  <div class="container-fluid px-5-percent">
+    <div class="bottom-image-grid my-5">
+      <div class="first-row">
+        <img src="../assets/images/grid-one@2x.jpg" alt="square-group" class="img-fluid">
+        <img src="../assets/images/grid-two@2x.jpg" alt="square-group" class="img-fluid">
+      </div>
+      <div class="second-row">
+        <img src="../assets/images/grid-three@2x.jpg" alt="square-group" class="img-fluid">
+        <img src="../assets/images/grid-four@2x.jpg" alt="square-group" class="img-fluid">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from 'vue-property-decorator';
+
+    @Component({
+        name: 'JobImageBlock',
+        components: {
+
+        }
+    })
+    export default class JobImageBlock extends Vue {
+
+    }
+</script>
+
+<style scoped lang="scss">
+  .bottom-image-grid {
+
+    .first-row {
+      display: flex;
+      height: 40vh;
+      width: 100%;
+
+      img:first-child {
+        flex-grow: 1;
+        object-fit: cover;
+        width: 50%;
+        @media(min-width: 961px) {
+          width: 70%;
+        }
+      }
+
+      img:last-child {
+        flex-shrink: 0;
+        object-fit: cover;
+        width: 50%;
+        @media(min-width: 961px) {
+          width: 30%;
+        }
+      }
+    }
+
+    .second-row {
+      display: flex;
+      height: 40vh;
+      width: 100%;
+
+      img:first-child {
+        flex-shrink: 0;
+        object-fit: cover;
+        width: 50%;
+        @media(min-width: 961px) {
+          width: 30%;
+        }
+      }
+
+      img:last-child {
+        flex-grow: 1;
+        object-fit: cover;
+        width: 50%;
+        @media(min-width: 961px) {
+          width: 70%;
+        }
+      }
+    }
+  }
+</style>
--- src/views/JobIndexPage.vue
+++ src/views/JobIndexPage.vue
@@ -13,21 +13,7 @@
       <JobFeaturedCard/>
     </div>
 
-
-    <!--    Bottom Image Grid-->
-    <div class="container-fluid px-5-percent">
-      <div class="bottom-image-grid my-5">
-        <div class="first-row">
-          <img src="../assets/images/grid-one@2x.jpg" alt="square-group" class="img-fluid">
-          <img src="../assets/images/grid-two@2x.jpg" alt="square-group" class="img-fluid">
-        </div>
-        <div class="second-row">
-          <img src="../assets/images/grid-three@2x.jpg" alt="square-group" class="img-fluid">
-          <img src="../assets/images/grid-four@2x.jpg" alt="square-group" class="img-fluid">
-        </div>
-      </div>
-    </div>
-
+    <JobImageBlock/>
     <FooterCard/>
     <CopyrightCard/>
   </div>
@@ -42,11 +28,13 @@
     import JobCategoryCard from '@/components/JobCategoryCard.vue';
     import JobFeaturedCard from '@/components/JobFeaturedCard.vue';
     import JobTitleCard from "@/components/JobTitleCard.vue";
+    import JobImageBlock from "@/components/JobImageBlock.vue";
     // import { gsap } from 'gsap';
 
     @Component({
         name: 'JobIndexPage.vue',
         components: {
+            JobImageBlock,
             JobTitleCard,
             JobFeaturedCard,
             JobCategoryCard,
@@ -68,54 +56,5 @@
 </script>
 
 <style scoped lang="scss">
-  .bottom-image-grid {
 
-    .first-row {
-      display: flex;
-      height: 40vh;
-      width: 100%;
-
-      img:first-child {
-        flex-grow: 1;
-        object-fit: cover;
-        width: 50%;
-        @media(min-width: 961px) {
-          width: 70%;
-        }
-      }
-
-      img:last-child {
-        flex-shrink: 0;
-        object-fit: cover;
-        width: 50%;
-        @media(min-width: 961px) {
-          width: 30%;
-        }
-      }
-    }
-
-    .second-row {
-      display: flex;
-      height: 40vh;
-      width: 100%;
-
-      img:first-child {
-        flex-shrink: 0;
-        object-fit: cover;
-        width: 50%;
-        @media(min-width: 961px) {
-          width: 30%;
-        }
-      }
-
-      img:last-child {
-        flex-grow: 1;
-        object-fit: cover;
-        width: 50%;
-        @media(min-width: 961px) {
-          width: 70%;
-        }
-      }
-    }
-  }
 </style>
