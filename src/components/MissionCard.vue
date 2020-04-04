aefad77ad33260eba261798de178a55e344bd7b6
mominriyadh <riyadh@codesign.com.bd>
2020-04-04 20:03:11 +0600

    fix and deploy

--- src/components/MissionCard.vue
+++ src/components/MissionCard.vue
@@ -1,49 +1,45 @@
 <template>
-
-    <div class="sq-mission-area">
-      <div class="sq-mission-title">
-        <h1>{{compData.title}}</h1>
-        <p>{{compData.subtitle}}</p>
-      </div>
-
-      <div class="row">
-        <div class="col text-center">
-          <div class="row text-left pt-4">
-            <div class="col-12 col-lg-4 mb-4 sq-mission-text pr-0 pr-md-5">
-              <h3>Our Mission</h3>
-              <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live
-                the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large
-                language ocean. A small river named Duden
-                flows by their place far far away.
-              </p>
-            </div>
-            <div class="col-12 col-lg-4 sq-mission-text pl-3 pl-lg-5">
-              <h3>Our Vision</h3>
-              <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, far far away, behind the
-                word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated
-                they live in Bookmarksgrove right at
-                the coast.
-              </p>
-            </div>
-
-            <div class="col-12 col-lg-4 sq-mission-text pl-3 pl-lg-5">
-              <h3>Our Values</h3>
-              <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, far far away, behind the
-                word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated
-                they live in Bookmarksgrove right at
-                the coast.
-              </p>
-            </div>
-          </div>
-          <div class="sq-mission-img col-12 p-0">
-            <img src="../assets/images/mission-ref.jpg" alt="" class="img-fluid">
-          </div>
-        </div>
-      </div>
-
-
+  <div class="sq-mission-area">
+    <div class="sq-mission-title">
+      <h1>{{compData.title}}</h1>
+      <p>{{compData.subtitle}}</p>
     </div>
 
+    <div class="row">
+      <div class="col text-center">
+        <div class="row text-left pt-4">
+          <div class="col-12 col-lg-4 mb-4 sq-mission-text pr-0 pr-md-5">
+            <h3>Our Mission</h3>
+            <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live
+              the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large
+              language ocean. A small river named Duden
+              flows by their place far far away.
+            </p>
+          </div>
+          <div class="col-12 col-lg-4 sq-mission-text pl-3 pl-lg-5">
+            <h3>Our Vision</h3>
+            <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, far far away, behind the
+              word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated
+              they live in Bookmarksgrove right at
+              the coast.
+            </p>
+          </div>
+
+          <div class="col-12 col-lg-4 sq-mission-text pl-3 pl-lg-5">
+            <h3>Our Values</h3>
+            <p>Separated they live in Bookmarksgrove right at the coast of the Semantics, far far away, behind the
+              word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated
+              they live in Bookmarksgrove right at
+              the coast.
+            </p>
+          </div>
+        </div>
+        <div class="sq-mission-img col-12 p-0">
+          <img src="../assets/images/mission-ref.jpg" alt="" class="img-fluid">
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
