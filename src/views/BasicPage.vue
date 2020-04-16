db30bfc2b1aaf0014d8a3fab1cbd1917d4b10943
mominriyadh <riyadh@codesign.com.bd>
2020-04-16 11:53:32 +0600

    fix and deploy

--- src/views/BasicPage.vue
+++ src/views/BasicPage.vue
@@ -1,30 +1,129 @@
 <template>
-  <div class="basic-page">
-          <component
-                v-for="comp in pageData.body"
-                :key="comp.id"
-                :is="comp.type"
-                :comp-data="comp.value">
-        </component>
+  <div v-show="pageData" :key="pageData.id" class="basic-page">
+
+    <!--=============================
+                Common Banner
+        =============================-->
+    <div class="common-banner-area">
+      <div class="container-fluid p-0">
+        <div class="common-banner js-bannerimage">
+          <img class="img-fluid" :src="HOST + pageData.banner_image.url" alt="square">
+
+          <div class="banner-content">
+            <h1 class="jstitle"> {{ pageData.title }}</h1>
+            <p class="jssubtitle"> {{ pageData.text }}</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+    <!--    End-->
+
+
+    <!--=================================
+           BasicPage Components Loop
+        =================================-->
+    <component
+            v-for="comp in pageData.body"
+            :key="comp.id"
+            :is="comp.type"
+            :comp-data="comp.value">
+    </component>
+    <!--    End-->
+
+
   </div>
 </template>
 
 <script lang="ts">
     import {Component, Prop, Vue} from 'vue-property-decorator';
-    import {BasicPageData} from '@/store/cms.types';
+    import {BasicPageData, TitleTextBannerImage} from '@/store/cms.types';
     import {HOST} from '@/global';
 
     @Component({
         name: 'BasicPage',
-        components: {}
+        components: {
+
+        }
     })
 
     export default class BasicPage extends Vue {
         @Prop() pageData!: BasicPageData;
-        HOST :string = HOST;
+        @Prop() compData!: TitleTextBannerImage;
+        HOST: string = HOST;
     }
 </script>
 
 <style scoped lang="scss">
 
+  /*Common Banner Style*/
+  .common-banner-area {
+    height: 50vh;
+    width: 100%;
+    display: block;
+    overflow: hidden;
+    margin-bottom: 1rem;
+    @media(min-width: 961px) {
+      margin-bottom: 5rem;
+    }
+  }
+
+  .common-banner {
+    position: relative;
+    width: 100%;
+    height: 50vh;
+
+    &::before {
+      width: 100%;
+      height: 100%;
+      position: absolute;
+      content: "";
+      top: 0;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background: linear-gradient(to bottom, rgba(0, 0, 0, 0.3) 100%, rgba(0, 0, 0, 0.5) 0%);
+    }
+
+    img {
+      width: 100%;
+      height: 50vh;
+      object-fit: cover;
+      -o-object-fit: cover;
+      object-position: center center;
+    }
+  }
+
+  .banner-content {
+    color: $white-color;
+    text-align: center;
+    width: 100%;
+    padding: 0 1rem;
+    position: absolute;
+    z-index: 2;
+    left: 50%;
+    top: 50%;
+    -webkit-transform: translate(-50%, -50%);
+    -moz-transform: translate(-50%, -50%);
+    -ms-transform: translate(-50%, -50%);
+    -o-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+
+    h1 {
+      font-size: calc(24px + (45 - 24) * ((100vw - 300px) / (1600 - 300)));
+      text-transform: capitalize;
+      font-family: $font-helvetica-bold;
+    }
+
+    p {
+      font-size: calc(14px + (16 - 14) * ((100vw - 300px) / (1600 - 300)));
+      font-family: $font-roboto;
+    }
+
+    @media (min-width: 992px) {
+      max-width: 1024px;
+    }
+  }
+
+  /*End Common Banner Style*/
 </style>
--- src/views/Page.vue
+++ src/views/Page.vue
@@ -7,27 +7,29 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import { Getter } from 'vuex-class';
-import { PAGE_DATA } from '@/store/getters.names';
-import { PageData } from '@/store/store.types';
-import HomePage from './HomePage.vue';
+    import {Component, Vue} from 'vue-property-decorator';
+    import {Getter} from 'vuex-class';
+    import {PAGE_DATA} from '@/store/getters.names';
+    import {PageData} from '@/store/store.types';
+    import HomePage from './HomePage.vue';
+    import BasicPage from '@/views/BasicPage.vue';
 
-@Component({
-  name: "Page",
-  components: {
-    HomePage: HomePage
-  },
-})
-export default class Page extends Vue {
-  @Getter(PAGE_DATA) pageData?: PageData | null;
+    @Component({
+        name: "Page",
+        components: {
+            HomePage: HomePage,
+            BasicPage: BasicPage
+        },
+    })
+    export default class Page extends Vue {
+        @Getter(PAGE_DATA) pageData?: PageData | null;
 
-  get pageType(): string | null {
-    if (this.pageData == null) {
-      return null;
+        get pageType(): string | null {
+            if (this.pageData == null) {
+                return null;
+            }
+            // "home.HomePage" will be "HomePage"
+            return this.pageData.meta.type.split(".")[1];
+        }
     }
-    // "home.HomePage" will be "HomePage"
-    return this.pageData.meta.type.split(".")[1];
-  }
-}
 </script>
