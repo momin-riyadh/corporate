55beafeff144c83ef2889a934c69d1407d054cac
mominriyadh <riyadh@codesign.com.bd>
2020-04-26 19:14:37 +0600

    job category page has been update

--- src/views/JobIndexPage.vue
+++ src/views/JobIndexPage.vue
@@ -288,50 +288,6 @@
 </script>
 
 <style scoped lang="scss">
-  .job-category-title {
-    margin-bottom: 1rem;
-    margin-top: 2rem;
-    @media all and (min-width: 601px) {
-      margin-bottom: 7rem;
-    }
-  }
-
-  .common-sub-title {
-    font-size: calc(14px + (18 - 14) * ((100vw - 300px) / (1600 - 300)));
-    color: #939393;
-    font-family: $font-roboto;
-    padding-top: 10px;
-  }
-
-  .common-title {
-    line-height: 1;
-    color: $brand-color;
-    font-family: $font-roboto;
-    font-weight: bold;
-    font-size: calc(24px + (36 - 24) * ((100vw - 300px) / (1600 - 300)));
-  }
-
-  .common-title span {
-    display: inline-block;
-    position: relative;
-    padding-bottom: 25px;
-  }
-
-  .common-title span:before {
-    content: "";
-    position: absolute;
-    height: .5rem;
-    border-bottom: 4px solid $brand-color;
-    bottom: 0;
-    width: 80px;
-  }
-
-  .common-title span:before {
-    left: 50%;
-    transform: translateX(-50%);
-  }
-
-
 
   .featured-job-area {
     margin-top: 1rem;
